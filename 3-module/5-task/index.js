f013a375b9818f12bba8e4941984329b2819300f
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2020-12-05 00:18:29 +0300

    function search min/max in the string

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let arrDataNoWSpace = str.split(' ');
+  let strData = arrDataNoWSpace.join(',');
+  let arrDataNoComma = strData.split(',');
+  
+  let arrNumber = arrDataNoComma.filter(function(item) {
+    let resultNumber;
+    if (Number(item) != NaN) resultNumber = Number(item);
+    return resultNumber;
+  });
+  return {
+    min: Math.min.apply(null, arrNumber),
+    max: Math.max.apply(null, arrNumber)
+  };
 }
