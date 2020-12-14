151f73b46e1d5572371bcdfb697e8884012ccaa6
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2020-12-14 22:09:54 +0300

    create class UserTable {}

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,47 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  }
+
+  remove(event) {
+    if (event.target.closest('button')) {
+      event.target.closest('tr').remove();
+    }
+  }
+
+  render() {
+    this.table = document.createElement('TABLE');
+
+    const thead = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>`;
+    this.table.insertAdjacentHTML('beforeend', thead);
+
+    const tbody = document.createElement('TBODY');
+    for (let row of this.rows) {
+      let rowContent = `
+        <tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button>X</button></td>
+        </tr>`;
+      tbody.insertAdjacentHTML('beforeend', rowContent);
+    };
+    this.table.append(tbody);
+
+    this.table.addEventListener('click', this.remove);
+
+    return this.table;
   }
 }
